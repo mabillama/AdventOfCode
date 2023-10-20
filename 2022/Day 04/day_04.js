5c8a20b7dd75db12b75d8f6ef8085a2a14ea2759
Marc Abillama <mabillama@gmail.com>
2023-10-20 15:29:14 +0200

    Answer for Day 4

---
+++ 2022/Day 04/day_04.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const readElfPairData = (path) => {
+  try {
+    return fs.readFileSync(path, "utf-8");
+  } catch (err) {
+    console.error(err);
+    return "";
+  }
+};
+
+const input = readElfPairData(__dirname + `\\day_04_input`);
+
+const isRangeContained = (mapInt) => {
+  const [a, b] = [mapInt[0], mapInt[1]];
+  const [x, y] = [mapInt[2], mapInt[3]];
+  return (a <= x && b >= y) || (x <= a && y >= b);
+  // if (mapInt[0] <= mapInt[2] && mapInt[1] >= mapInt[3]) {
+  //   return 1;
+  // } else if (mapInt[2] <= mapInt[0] && mapInt[3] >= mapInt[1]) {
+  //   return 1;
+  // } else {
+  //   return 0;
+  // }
+};
+
+const totalPairsContained = input
+  .split("\r\n")
+  .map((entry) =>
+    entry.split(",").flatMap((range) => range.split("-").map(Number))
+  )
+  .map((el) => isRangeContained(el))
+  .reduce((acc, val) => acc + val, 0);
+
+console.log(totalPairsContained);
+
+// Part 2
+
+const doOverlap = (mapInt) => {
+  const [a, b] = [mapInt[0], mapInt[1]];
+  const [x, y] = [mapInt[2], mapInt[3]];
+  return (a <= x && b >= x) || (x <= a && y >= a);
+};
+
+const totalPairsOverlapped = input
+  .split("\r\n")
+  .map((entry) =>
+    entry.split(",").flatMap((range) => range.split("-").map(Number))
+  )
+  .map((el) => doOverlap(el))
+  .reduce((acc, val) => acc + val, 0);
+
+console.log(totalPairsOverlapped);
